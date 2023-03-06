976387133829084d58b555d28b1898817a1273d3
apolpdga <76147370+apolpdga@users.noreply.github.com>
2023-03-06 16:54:07 +0800

    docs: 重構註解

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //定義要使用的
 app.set('view engine', 'handlebars') //設定 view engine 是 handlebars
 
 // 資料庫設定
-// 載入 Todo model
+// 載入 URL model
 const URL = require('./models/shortenURL')
 //載入mongoose設定
 require('./config/mongoose')
@@ -28,7 +28,7 @@ require('./config/mongoose')
 //產生短網址程式
 const getShortURL = require('./utilities/getShortURL')
 
-// 路由設定
+// 路由前置設定
 // 引用 body-parser，用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -37,52 +37,56 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const methodOverride = require('method-override')
 app.use(methodOverride('_method')) //將每筆路由使用 methodOverride 進行前置處理
 
-// 首頁
+// 路由設定
+
+// 首頁 -> 短網址轉址介面
 app.get('/', (req, res) => {
-  res.render('index') // 將資料傳給 index 樣板
+  res.render('index') 
 })
 
-// 短網址資料庫
+// 短網址資料庫 -> 瀏覽、修改短網址資訊
 app.get('/urls', (req, res) => {
-  URL.find() // 取出 URL model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(urls => res.render('urldb', { urls: urls })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
+  URL.find() // 
+    .lean() 
+    .then(urls => res.render('urldb', { urls: urls })) 
+    .catch(error => console.error(error)) 
 })
 
-//短網址轉址
+//短網址轉址 -> 將輸入的短網址轉址至原網址
 app.get('/:short', (req, res) => {
-  if (req.params.short === 'favicon.ico') {  //防止進入首頁時自動執行此路由
+	
+  //防止進入首頁時自動執行此路由
+  if (req.params.short === 'favicon.ico') {  
     // console.log(req.params.short)
-    return res.redirect('/') // 將資料傳給 index 樣板
+    return res.redirect('/') 
   }
 
-  URL.findOne({ shorted_url: req.params.short }) // 取出 URL model 裡的shorten_url
+  // 轉址至原網址(利用短網址找出原網址)
+  URL.findOne({ shorted_url: req.params.short }) 
     .then(data => {
       res.redirect(data.original_url)
     })
     .catch(error => console.error(error))
 })
 
-// 將取得的網址和產生的短網址，一併存入資料庫，並導回首頁
+// 將取得的網址和新產生的短網址，一併存入資料庫，並導回首頁
 app.post('/', (req, res) => {
   //如果使用者輸入的網址已存在資料庫中，則回傳相對應的短網址
 
   //如果產生的短網址已在資料庫中，則重新產生
   const shortedURL = getShortURL()  //亂數產生短網址
 
-  //資料寫入資料庫
+  //資料寫入資料庫，回導回首頁(呈現短網址的介面)
   URL.create({ original_url: req.body.originalURL, shorted_url: shortedURL })     // 資料新增至資料庫
     .then((data) => {
-      res.render('index', {
-        origin: req.headers.origin,
-        shorted_url: data.shorted_url,
-      }) // 新增完成後導回首頁
+      res.render('index', {            
+        origin: req.headers.origin,    // 網站首頁網址
+        shorted_url: data.shorted_url, // 短網址
+      }) 
     })
     .catch(error => console.log(error))
   // 使用then時需要返回一個結果，而當使用 arrow function, 例如() => x 其實就是() => { return x; }，故會返回結果
 })
-
 
 //delete
 app.delete('/urls/:id', (req, res) => {
